949c34143842bc26eb421c0c8f12415cdecd90ac
Flávio Pangrácio <flaviohugo.14@gmail.com>
2019-02-20 12:56:38 -0300

    aumentando limite pra 4 MB

--- src/config/multer.js
+++ src/config/multer.js
@@ -40,7 +40,7 @@ module.exports = {
     dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
     storage: storageTypes[process.env.STORAGE_TYPE],
     limits: {
-        fileSize: 2 * 1024 * 1024,
+        fileSize: 4 * 1024 * 1024,
     },
     fileFilter: (req, file, cb) => {
         const allowedMimes = [
